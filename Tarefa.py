9705a58ce501536b5ecd21e61dfed3c650f1daad
Gabriel Sá <123607677+GabrielSa271@users.noreply.github.com>
2023-02-08 23:17:00 +0000

    Primeiro código git

---
+++ Tarefa.py
@@ -0,0 +1,55 @@
+#Exercicio 1
+'''
+n1 = float(input("Nota 1: ",))
+n2 = float(input("Nota 2: ",))
+n3 = float(input("Nota 3: ",))
+
+me = ((n1+n2+n3)/3)
+
+print("A média do aluno é: ", me)
+
+ma = ((n1+n2*2+n3*3+me)/7)
+
+print("A média de aproveitamento é: ", ma)
+
+if ma>=9:
+  print("A média do aluno está positiva")
+if ma<4:
+   print("A média do aluno está negativa")
+else:
+   print("Você está dentro da média")
+
+#Exercicio 2
+nome = str(input("Insira seu Nome: "))
+endereço = str(input("Insira seu endereço: "))
+cep = int(input("Insira seu código postal: "))
+telefone = int(input("Insira seu telefone (Ex. (ddd) xxxxx-xxxx): "))
+
+print("nome: ", (nome))
+print("endereço: ", (endereço))
+print("CEP e Telefone: ", (cep), (telefone))
+
+#Exercicio 3
+print("xxxxx")
+print("x   x")
+print("x   x")
+print("x   x")
+print("xxxxx")
+
+#Exercicio 4
+aluno = str(input("Insira o nome do Aluno: "))
+nota = int(input("Insira a nota do Aluno: "))
+
+print("Aluno      Nota")
+print("======     ====")
+print((aluno),"       ",(nota))'''
+
+#Exercicio 5
+print("0 - Fim")
+print("1 - Inclui")
+print("2 - Altera")
+print("3 - Exclui")
+print("4 - Consulta")
+
+varia = int(input("Insira um dos valores: "))
+
